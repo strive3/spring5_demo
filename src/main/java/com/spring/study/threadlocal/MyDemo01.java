6cd09ee4ee2df447242f6c274469abe653bfe6cc
Xiaopeng <961844168@qq.com>
2021-02-24 12:38:39 +0800

    threadlocal的demo

---
+++ src/main/java/com/spring/study/threadlocal/MyDemo01.java
@@ -0,0 +1,51 @@
+package com.spring.study.threadlocal;
+
+import lombok.Data;
+
+/**
+ * @Author duxiaopeng
+ * @Date 2021/2/24 11:29 上午
+ * @Description 线程隔离：
+ *              在多线程并发的场景下，每个线程的变量都是相互独立
+ *              线程A： 设置（变量1）    获取（变量1）
+ *              线程B： 设置（变量2）    获取（变量2）
+ *              ThreadLocal:
+ *                  1.set():    将变量绑定到当前线程中
+ *                  2.get():    获取当前线程绑定的变量
+ */
+public class MyDemo01 {
+
+    ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    private String content;
+
+    public String getContent() {
+//        return content;
+        String s = threadLocal.get();
+        return s;
+    }
+
+    public void setContent(String content) {
+//        this.content = content;
+        //将变量绑定到ThreadLocal
+        threadLocal.set(content);
+    }
+
+    public static void main(String[] args) {
+        MyDemo01 demo01 = new MyDemo01();
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(()->{
+                /**
+                 * @Description 每个线程存一个变量，过会取出这个变量
+                 */
+                demo01.setContent(Thread.currentThread().getName() + "的数据");
+                System.out.println("--------------------");
+                System.out.println(Thread.currentThread().getName() + "--->" + demo01.getContent());
+            });
+            thread.setName("线程" + i);
+            thread.start();
+        }
+
+    }
+
+}
